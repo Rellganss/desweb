c31d206440dc816b97935e3adc1292d521a80468
Rellganss <11211029@student.itk.ac.id>
2023-12-01 15:22:28 +0800

    perbaikan import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Navbar from './components/Navbar'; // Pastikan path-nya sesuai
-import Footer from './components/Footer';
-import CardList from './components/Cardlist';
 import Login from './pages/Login';
 
 
 const App = () => {
-  const cardCount = 10; 
   return (
     <div>
       <Login/>
