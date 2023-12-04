ecd443a445f2f467aad5f9243bb2646c46324248
shafaps <103978919+shafaps@users.noreply.github.com>
2023-12-04 23:49:33 +0800

    fix ui landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import CardList from '../components/Cardlist';
 import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-
+import NavbarBaru from '../components/NavbarBaru';
 
 export default function Landing(){
   const cardCount = 20; 
   return (
-    <div>
-      <Navbar />
+    <div className="w-full">
+      <NavbarBaru />
       <section className='content mt-[100px] mb-[20px]  '>
         <CardList cardCount={cardCount} />
       </section>
