eb3c9c9314e849d36d589ca80849e82aff2254d6
shafaps <103978919+shafaps@users.noreply.github.com>
2023-12-04 23:48:55 +0800

    responsive navbar dan footer

---
+++ src/components/NavbarBaru.jsx
@@ -0,0 +1,77 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
+import freedersLogo from '../assets/logo freeders.png';
+
+const NavbarBaru = () => {
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleLandingClick = () => {
+        // Navigate to the Landing page
+        window.location.href = '/landing';
+    };
+
+    const handleDaftarClick = () => {
+        // Handle Daftar click action
+        window.location.href = '/daftarfreelancer';
+
+    };
+
+    const handleTentangClick = () => {
+        // Handle Tentang click action
+        window.location.href = '/tentang';
+
+    };
+
+    const handleKontakClick = () => {
+        // Handle Kontak click action
+        window.location.href = '/kontak';
+
+    };
+
+    const handleLogOutClick = () => {
+        // Handle LogOut click actionhttp://localhost:5173/
+        window.location.href = 'http://localhost:5173/';
+
+    };
+
+    return (
+        <div>
+        <div className="flex flex-col md:flex-row w-full  items-center bg-teal-700">
+            <div className="flex flex-col md:flex-row items-center w-full  bg-teal-700">
+            <div className="flex md:flex-row items-center sm:w-full xl:w-1/3 md:w-full lg:md-full w-full bg-teal-700 ">
+                <img src={freedersLogo} alt="logo freeders" onClick={handleLandingClick} className="w-[100px] h-[100px] mr-2 cursor-pointer" />
+                <input
+                type="text"
+                placeholder="Search"
+                className="p-2 border rounded-full focus:outline-none w-64"
+                />
+                <div className="md:hidden ml-auto p-4 text-white cursor-pointer" onClick={toggleDropdown}>
+                <FontAwesomeIcon icon={faBars} />
+                </div>
+            </div>
+            <div className={`md:flex mr-auto flex-col md:flex-row gap-[30px]  w-full ${isDropdownOpen ? 'block' : 'hidden'}`}>
+                <div className="md:flex p-4 text-white font-[Poppins] cursor-pointer" onClick={handleDaftarClick}>
+                Daftar Sebagai Freelancer
+                </div>
+                <div className="md:flex p-4 text-white font-[Poppins] cursor-pointer" onClick={handleTentangClick}>
+                Tentang
+                </div>
+                <div className="md:flex p-4 text-white font-[Poppins] cursor-pointer" onClick={handleKontakClick}>
+                Kontak
+                </div>
+                <div className="md:flex p-4 text-white font-[Poppins] ml-auto cursor-pointer" onClick={handleLogOutClick}>
+                LogOut
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    );
+    };
+
+    export default NavbarBaru;
